6c3578baee33540b71444cfc8141033faf300e64
tapiamicaela <tapiamikaa521@gmail.com>
2025-07-04 01:32:41 -0300

    mostar y agregar alumnos con archivos

--- TP_N°6.py
+++ TP_N°6.py
@@ -1,132 +1,69 @@
-"""1) Hacer un programa que gestiones datos para una escuela.
-El programa tiene que ser capaz de:
-    a) Llevar un registro de todos los datos de alumnos de la escuela (Nombre,
-    Apellido, fecha de nacimiento, DNI, Nombre de Tutor, registro de todas las
-    notas, cantidad de faltas, cantidad de amonestaciones recibidas.
-    Recomendación: Para llevar un registro de estos dato se puede
-    utilizar un diccionario estructurado de la siguiente manera:
-    {
-    “Alumnos” : [alumno1,alumno2,alumno3 ]
-    }
-    Donde cada alumno es otro diccionario estructurado de la
-    siguiente forma:
-    {
-    “Nombre”: nombre de alumno,
-    “Apellido” : apellido de alumno,
-    “DNI” : DNI de alumno
-    “Fecha de nacimiento”, fecha de nacimiento de alumno,
-    “Tutor” : nombre y apellido de tutor,
-    “Notas” : todas las notas del alumno,
-    “Faltas” : cantidad de faltas,
-    “amonestaciones” : cantidad de amonestaciones
-    }
-    En esta estructura:
-    Datos = {
-    “Alumnos” : [alumno1,alumno2,alumno3 ]
-    }
-    Para acceder por ejemplo al numero de DNI del tercer alumno
-    podríamos hacer algo así:
-    Datos[“Alumnos”][2][“DNI”]
-Este es un ejemplo de estructura, se puede cambiar
-completamente o hacer algunos cambios sobre el para mejorar el
-orden (si lo consideran necesario)
-b) Mostrar los datos de cada alumno
-c) Modificar los datos de los alumnos
-d) Agregar alumnos
-e) Expulsar alumnos"""
+def mostrar_alumno(dni,archivo):
+    with open(archivo,"r") as file:
+        lineas=file.readlines()
+        for alumno in lineas:
+            dato=alumno.split(";")
+            if dato[2]==dni:
+                print(f"Nombre: {dato[0]}")
+                print(f"Apellido: {dato[1]}")
+                print(f"DNI: {dato[2]}")
+                print(f"Fecha de nacimiento: {dato[3]}")
+                print(f"Tutor: {dato[4]}")
+                print(f"Notas: {dato[5]}")
+                print(f"Faltas: {dato[6]}")
+                print(f"Amonestaciones: {dato[7]}")
+                print("-------------------------------------")
+                return
+        print("Alumno no encontrado.")
 
 
-datos_alumnos={
-    'Alumnos' : [
-        {
-        'Nombre': "Milagros",
-        'Apellido' : "Tapia",
-        'DNI' : "43441001",
-        'Fecha de nacimiento': "17/06/2001",
-        'Tutor' : "Graciela Cutipa",
-        'Notas' : [10,9,8,7],
-        'Faltas' : 2,
-        'Amonestaciones' : 0
-        },
-
-        {
-        'Nombre': "Juan",
-        'Apellido' : "Perez",
-        'DNI' : "46441023",
-        'Fecha de nacimiento': "15/07/2004",
-        'Tutor' : "Osar Perez",
-        'Notas' : [7,8,8,7],
-        'Faltas' : 6,
-        'Amonestaciones' : 2
-        },
-
-        {
-        'Nombre': "Luis",
-        'Apellido' : "Martinez",
-        'DNI' : "46229209",
-        'Fecha de nacimiento': "27/08/2006",
-        'Tutor' : "Lucia Martinez",
-        'Notas' : [6,7,9,6],
-        'Faltas' : 4,
-        'Amonestaciones' : 1
-        },
-
-        {
-        'Nombre': "Rodrigo",
-        'Apellido' : "Fernandez",
-        'DNI' : "46229118",
-        'Fecha de nacimiento': "27/01/2006",
-        'Tutor' : "Ana Martinez",
-        'Notas' : [6,8,10,6],
-        'Faltas' : 5,
-        'Amonestaciones' : 4
-        },
-    ]
-}
-
-
-def mostrar_alumno(dni,datos_alumnos):
-    for alumno in datos_alumnos['Alumnos']: 
-        if alumno["DNI"]==dni:
-            for dato in alumno:
-                print(f"{dato}: {alumno[dato]}")
-            return
-    print("Alumno no encontrado.")
-
-
-def mostrar_todos_alumnos(datos_alumnos):
-    for alumno in datos_alumnos['Alumnos']: 
-        for dato in alumno:
-            print(f"{dato}: {alumno[dato]}")
-        print("-------------------------------------")
+def mostrar_todos_alumnos(archivo):
+    with open(archivo, "r") as file:
+        lineas=file.readlines()
+        print("Listado de todos los alumnos:\n")
+        for alumno in lineas:
+            dato=alumno.strip().split(";")
+            print(f"Nombre: {dato[0]}")
+            print(f"Apellido: {dato[1]}")
+            print(f"DNI: {dato[2]}")
+            print(f"Fecha de nacimiento: {dato[3]}")
+            print(f"Tutor: {dato[4]}")
+            print(f"Notas: {dato[5]}")
+            print(f"Faltas: {dato[6]}")
+            print(f"Amonestaciones: {dato[7]}")
+            print("-------------------------------------")
 
 
 def ingresar_datos():
-    nuevo_alumno={}
-    nuevo_alumno['Nombre'] = input(f"Ingrese el nombre del alumno: ")
-    nuevo_alumno['Apellido'] = input(f"Ingrese el apellido del alumno: ")
-    nuevo_alumno['DNI'] = input(f"Ingrese el DNI del alumno: ")
-    nuevo_alumno['Fecha de nacimiento'] = input(f"Ingrese la Fecha de nacimiento del alumno: ")
-    nuevo_alumno['Tutor'] = input(f"Ingrese el nombre del tutor: ")
+    dato=[]
+    dato.append(input(f"Ingrese el nombre del alumno: "))
+    dato.append(input(f"Ingrese el apellido del alumno: "))
+    dato.append(input(f"Ingrese el DNI del alumno: "))
+    dato.append(input(f"Ingrese la Fecha de nacimiento del alumno: "))
+    dato.append(input(f"Ingrese el nombre del tutor: "))
     notas_str = input("Ingrese las notas separadas por comas (ej: 10,9,8): ")
-    lista=[]
-    for nota in notas_str.split(','):
-        lista.append(int(nota))
-    nuevo_alumno["Notas"] = lista
-    nuevo_alumno['Faltas'] = int(input(f"Ingrese el numero de faltas: "))
-    nuevo_alumno['Amonestaciones'] = int(input(f"Ingrese el numero de amonestaciones: "))
+    dato.append(f"[{notas_str}]")
+    dato.append(input(f"Ingrese el numero de faltas: "))
+    dato.append(input(f"Ingrese el numero de amonestaciones: "))
 
-    return nuevo_alumno
+    return dato
 
 
-def agregar_alumno(datos_alumnos):
+def agregar_alumno(archivo):
     nuevo_alumno=ingresar_datos()
-    for alumno in datos_alumnos['Alumnos']:
-        if alumno["DNI"]==nuevo_alumno['DNI']:
-            print("Error: este alumno ya existe")
-            return
-    datos_alumnos['Alumnos'].append(nuevo_alumno)
-    print("Alumno agregado correctamente.")
+    nuevo_dni=nuevo_alumno[2]
+    with open(archivo,"r") as file:
+        lineas=file.readlines()
+        for alumno in lineas:
+            if alumno !="":
+                dato=alumno.split(";")
+                if dato[2]==nuevo_dni:
+                    print("Error: este alumno ya existe")
+                    return
+        alumno=";".join(nuevo_alumno) + "\n"
+        with open(archivo,"a") as file:
+                file.write(alumno)
+        print("Alumno agregado correctamente.")
 
 
 def modificar_alumno(dni,datos_alumnos):
@@ -189,24 +126,24 @@ while True:
     if operacion==1:
         dni= input("Ingrese el DNI del alumno que desee ver la informacion: ")
         print("\n")
-        mostrar_alumno(dni,datos_alumnos)
+        mostrar_alumno(dni,r"C:\repositorioGit\legajodealumnos.txt")
     elif operacion==2:
         print("\n")
-        mostrar_todos_alumnos(datos_alumnos)
+        mostrar_todos_alumnos(r"C:\repositorioGit\legajodealumnos.txt")
     elif operacion==3:
-        agregar_alumno(datos_alumnos)
+        agregar_alumno(r"C:\repositorioGit\legajodealumnos.txt")
         print("\n")
-        mostrar_todos_alumnos(datos_alumnos)
+        mostrar_todos_alumnos(r"C:\repositorioGit\legajodealumnos.txt")
     elif operacion==4:
         dni= input("Ingrese el DNI del alumno que desee modificar: ")
-        modificar_alumno(dni,datos_alumnos)
+        modificar_alumno(dni,r"C:\repositorioGit\legajodealumnos.txt")
         print("\n")
-        mostrar_todos_alumnos(datos_alumnos)
+        mostrar_todos_alumnos(r"C:\repositorioGit\legajodealumnos.txt")
     elif operacion==5:
         dni= input("Ingrese el DNI del alumno que desee eliminar: ")
-        eliminar_alumno(dni,datos_alumnos)
+        eliminar_alumno(dni,r"C:\repositorioGit\legajodealumnos.txt")
         print("\n")
-        mostrar_todos_alumnos(datos_alumnos)
+        mostrar_todos_alumnos(r"C:\repositorioGit\legajodealumnos.txt")
     elif operacion==6:
         print("Saliendo del programa, ¡Hasta luego!") 
         break
